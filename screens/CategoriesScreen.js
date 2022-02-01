f03a9262e5dfcb5574138fd952bcc21f0d0b7672
Ganesh Balaji Radhakrishnan <ganesh.krishnan237@gmail.com>
2022-02-01 11:45:08 +0100

    used diff method in stack nav

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,26 +1,28 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native'
+import React from 'react'
 
 const CategoriesScreen = (props) => {
   return (
     <View style={styles.container}>
       <Text>This is CategoriesScreen</Text>
-      <Button title="click to meals" onPress={()=>{
+      <Button
+        title="click to meals"
+        onPress={() => {
           //else props.navigation.navigate('SomeIdentifier');
-          props.navigation.navigate({routeName : 'CategoriesMeals'})
-      }}/>
+          props.navigation.navigate({ routeName: 'CategoriesMeals' })
+        }}
+      />
     </View>
-  );
-};
+  )
+}
 
-export default CategoriesScreen;
+export default CategoriesScreen
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: 'white'
-    }
-});
- 
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+  },
+})
