c6fc133cde6aa4fc60d94c42cb0c474e62bc8792
Ganesh Balaji Radhakrishnan <ganesh.krishnan237@gmail.com>
2022-02-01 21:12:42 +0100

    added categories screen nav

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,19 +1,49 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+} from 'react-native'
 import React from 'react'
 
+import { CATEGORIES } from '../data/dummy-data'
+import Colors from '../constants/Colors'
+
 const CategoriesScreen = (props) => {
-  return (
-    <View style={styles.container}>
-      <Text>This is CategoriesScreen</Text>
-      <Button
-        title="click to meals"
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
         onPress={() => {
-          //else props.navigation.navigate('SomeIdentifier');
           props.navigation.navigate({ routeName: 'CategoriesMeals' })
         }}
-      />
-    </View>
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
   )
+}
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meals Categories',
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : 'white',
+  },
+  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
 }
 
 export default CategoriesScreen
@@ -25,4 +55,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     color: 'white',
   },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+  },
 })
+
+/*  <View style={styles.container}>
+      <Text>This is CategoriesScreen</Text>
+      <Button
+        title="click to meals"
+        onPress={() => {
+          //else props.navigation.navigate('SomeIdentifier');
+          props.navigation.navigate({ routeName: 'CategoriesMeals' })
+        }}
+      />
+    </View> */
